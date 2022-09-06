a3657a8cff8aeb1899b711b22ac83a1146f632a0
IUTA <vasavaseckin801@gmail.com>
2022-09-06 21:08:00 +0300

    Main function definition

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+
+NUM_DIGITS = 3
+MAX_GUESSES = 10
+
+def main():
+
+def getSecretNum():
+
+'''
+    Возвращает строку с подсказками pico, fermi и bagels
+    для полученной на входе пары из догадки и секретного числа
+'''
+def getClues(guess, secretNum):
+
+# Если программа не импортируется, а запускается, производим запуск:
+if __name__ == '__main__':
+    main()
